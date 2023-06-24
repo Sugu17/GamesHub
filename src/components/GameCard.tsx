fbe14574554cda764f962141611220d8ed320744
Wade Wilson <sugumarsg17@@gmail.com>
2023-06-24 06:03:59 +0530

    Added display of game platforms using text

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,11 @@
-import { Card, CardBody, CardHeader, Heading, Image } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 
 interface Props {
@@ -9,9 +16,10 @@ export default function GameCard(props: Props) {
   return (
     <Card borderRadius={12} overflow={"hidden"}>
       <Image src={props.game.background_image} />
-      <CardHeader>
+      <CardHeader paddingY={0} paddingTop={3}>
         <Heading fontSize={"2xl"}>{props.game.name}</Heading>
       </CardHeader>
+      <CardBody></CardBody>
     </Card>
   );
 }

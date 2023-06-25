1ec69136605f6e5c33e696c19abf632fd4f46a0a
Wade Wilson <sugumarsg17@@gmail.com>
2023-06-25 20:17:54 +0530

    Added  Genre Image to genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,24 @@
-import { ListItem, UnorderedList } from "@chakra-ui/react";
+import { ListItem, List, HStack, Image, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenres";
+import getCroppedImageURL from "../services/image-url";
 
 export default function GenreList() {
   const genreHookObj = useGenre();
   const { genres } = genreHookObj;
   return (
-    <UnorderedList listStyleType={"none"}>
+    <List listStyleType={"none"}>
       {genres.map((genre) => (
-        <ListItem key={genre.id}>{genre.name}</ListItem>
+        <ListItem key={genre.id} paddingY={3}>
+          <HStack spacing={3}>
+            <Image
+              boxSize={10}
+              borderRadius={8}
+              src={getCroppedImageURL(genre.image_background)}
+            />
+            <Text fontSize={"lg"}>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </UnorderedList>
+    </List>
   );
 }
